a94186c2064a0744eca2bee6c22fad2b90a82dc4
tobinguyenx <nguyentaitino@gmail.com>
2026-03-23 18:30:02 +1100

    update const

---
+++ script.js
@@ -0,0 +1,5 @@
+const input = document.getElementById("input-el");
+const buttonconvert = document.getElementById("convert-btn");
+const length = document.getElementById("length-el");
+const volume = document.getElementById("volume-el");
+const mass = document.getElementById("mass-el");
