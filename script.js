2a2cace92bcdcc8bdb32c7228d0ec2eb76b39162
tobinguyenx <nguyentaitino@gmail.com>
2026-03-23 19:20:47 +1100

    add event listener click for  button convert

--- script.js
+++ script.js
@@ -8,15 +8,25 @@ const meterToFeet = 3.28084;
 const literToGallon = 0.264172;
 const kiloToPound = 2.20462;
 
-function rounddowntothree() {
+function rounddowntothree(num) {
   return (Math.floor(num * 1000) / 1000).toFixed(3);
 }
 
 function render() {
   const giatri = input.value;
-  const giatriso = Number(giatri);
+  const value = Number(giatri);
 
   const metertofeet = rounddowntothree(value * meterToFeet);
-  const litertGallon = rounddowntothree(value * literToGallon);
+  const litertgallon = rounddowntothree(value * literToGallon);
   const kilotopound = rounddowntothree(value * kiloToPound);
+
+  const feetometer = rounddowntothree(value / meterToFeet);
+  const gallontoliter = rounddowntothree(value / literToGallon);
+  const poundtokilo = rounddowntothree(value / kiloToPound);
+
+  length.textContent = `${value} meters = ${metertofeet} feet`;
 }
+
+buttonconvert.addEventListener("click", () => {
+  render();
+});
